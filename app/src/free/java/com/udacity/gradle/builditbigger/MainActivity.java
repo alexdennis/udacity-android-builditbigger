e5056fd9f6ac28cec0ae065badf70c440d9d5e5a
Alex Dennis <alex.dennis@gmail.com>
2015-07-26 22:58:49 -0400

    Add Interstitial Ad, Add Loading Indicator

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,20 +6,49 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.carltondennis.jokedisplay.JokeDisplay;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 public class MainActivity extends ActionBarActivity implements TellJokeAsyncTask.Callback{
 
+    InterstitialAd mInterstitialAd;
     private TellJokeAsyncTask asyncTask;
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.interstitial_banner_add_unit_id));
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                fetchJoke();
+                requestNewInterstitial();
+            }
+        });
+
+        requestNewInterstitial();
         setContentView(R.layout.activity_main);
+
+        spinner = (ProgressBar) findViewById(R.id.spinner);
+        spinner.setVisibility(View.GONE);
     }
 
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,6 +73,17 @@ public class MainActivity extends ActionBarActivity implements TellJokeAsyncTask
     }
 
     public void tellJoke(View view){
+
+        if (mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            fetchJoke();
+        }
+    }
+
+    private void fetchJoke() {
+        spinner.setVisibility(View.VISIBLE);
+
         asyncTask = new TellJokeAsyncTask(this);
         asyncTask.execute();
     }
@@ -59,6 +99,8 @@ public class MainActivity extends ActionBarActivity implements TellJokeAsyncTask
 
     @Override
     public void onJokeResponse(String joke) {
+        spinner.setVisibility(View.GONE);
+        
         Intent i = new Intent(this, JokeDisplay.class);
         i.putExtra(JokeDisplay.JOKE_TEXT, joke);
         startActivity(i);
